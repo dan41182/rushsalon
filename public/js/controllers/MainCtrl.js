61a32abf7de4ab0a47d920a5d4929e3d9a369cad
da <dannyhaitran@outlook.com>
2015-08-21 08:45:21 -0700

    Changed slider

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,8 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
     $('.slider').slider({full_width: false, height: 600});
     $('.slider').slider('start');
-    $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav({
+        closeOnClick: true
+    });
 
 });
